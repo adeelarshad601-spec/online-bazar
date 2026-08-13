7a892bb410c7016726cf56d0130aa1f18fce6d34
Adeel Arshad <adeelarshad601@gmail.com>
2026-08-14 00:51:18 +0500

    *

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,15 @@
+import { QueryProvider } from "@/providers/query-provider";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <QueryProvider>{children}</QueryProvider>
+      </body>
+    </html>
+  );
+}
